8796ed76022a7f85b6915a8d1b4667ac28dc4047
QuanNguyenDong <nguyendongquan031102@gmail.com>
2022-09-12 18:50:49 +1000

    Build 10 features

---
+++ assets/main.js
@@ -0,0 +1,270 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const PLAYER_STORAGE_KEY = 'Just_Player'
+
+const player = $('.player')
+const cd = $('.cd')
+const heading = $('header h2')
+const cdThumb = $('.cd-thumb')
+const audio = $('#audio')
+const playBtn = $('.btn-toggle-play')
+const progress = $('#progress')
+const nextBtn = $('.btn-next')
+const prevBtn = $('.btn-prev')
+const randomBtn = $('.btn-random')
+const repeatBtn = $('.btn-repeat')
+const playlist = $('.playlist')
+
+const app = {
+    currentIndex: 0,
+    isPlaying: false,
+    isRandom: false,
+    isRepeat: false,
+    config: JSON.parse(localStorage.getItem(PLAYER_STORAGE_KEY)) || {},
+    songs: [
+        {
+            name: 'Vicetone',
+            singer: 'Nevada',
+            path: './assets/music/vicetone.mp3',
+            image: 'https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg',
+        },
+        {
+            name: 'See Tinh',
+            singer: 'Hoang Thuy Linh',
+            path: './assets/music/seetinh.mp3',
+            image: 'https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg',
+        },
+        {
+            name: 'Monsters',
+            singer: 'Katie Sky',
+            path: './assets/music/monster.mp3',
+            image: 'https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg',
+        },
+        {
+            name: 'Decade of POP',
+            singer: 'Raftaar x Nawazuddin Siddiqui',
+            path: './assets/music/celebrate.mp3',
+            image: 'https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg',
+        },
+    ],
+    setConfig: function(key, value) {
+        this.config[key] = value;
+        localStorage.setItem(PLAYER_STORAGE_KEY, JSON.stringify(this.config))
+    },
+    render: function() {
+        const htmls = this.songs.map((song, index) => {
+            return `
+                <div class="song${index === this.currentIndex ? " active" : ""}" data-index=${index}>
+                    <div class="thumb" style="background-image: url('${song.image}')">
+                    </div>
+                    <div class="body">
+                    <h3 class="title">${song.name}</h3>
+                    <p class="author">${song.singer}</p>
+                    </div>
+                    <div class="option">
+                    <i class="fas fa-ellipsis-h"></i>
+                    </div>
+                </div>
+            `
+        })
+        playlist.innerHTML = htmls.join('\n')
+    },
+    defineProperties: function() {
+        Object.defineProperty(this, 'currentSong', {
+            get: function() {
+                return this.songs[this.currentIndex]
+            }
+        })
+    },
+    handleEvents: function() {
+        const _this = this
+        const cdWidth = cd.offsetWidth
+
+        // Handle CD rotate / stop
+        const cdThumbAnimate = cdThumb.animate([
+            { transform: 'rotate(360deg)' }
+        ], {
+            duration: 10000,    // 10seconds
+            iterations: Infinity
+        })
+        cdThumbAnimate.pause()
+
+        // Handle zoom in/ zoom out CD
+        document.onscroll = function() {
+            const scrollTop = window.scrollY || document.documentElement.scrollTop
+            const newCdWidth = cdWidth - scrollTop
+
+            cd.style.width = newCdWidth > 0 ? newCdWidth + 'px' : 0
+            cd.style.opacity = newCdWidth / cdWidth
+        }
+
+        // Handle when click play
+        playBtn.onclick = function() {
+            if (_this.isPlaying) {
+                audio.pause()
+            } else {
+                audio.play()
+            }
+        }
+
+        // When song is played
+        audio.onplay = function() {
+            _this.isPlaying = true
+            player.classList.add('playing')
+            cdThumbAnimate.play()
+        }
+
+        // When song is paused
+        audio.onpause = function() {
+            _this.isPlaying = false
+            player.classList.remove('playing')
+            cdThumbAnimate.pause()             
+        }
+
+        // when song progresses
+        audio.ontimeupdate = function() {
+            if (audio.duration) {
+                const progressPercent = Math.floor(audio.currentTime / audio.duration * 100)
+                progress.value = progressPercent
+            }
+        }
+
+        // Handle when play back
+        progress.onchange = function(e) {
+            const seekTime = audio.duration / 100 * e.target.value
+            audio.currentTime = seekTime
+        }
+
+        // When next song
+        nextBtn.onclick = function() {
+            if (_this.isRandom) {
+                _this.playRandomSong()
+            } else {
+                _this.nextSong()
+            }
+
+            audio.play()
+            _this.render()
+            _this.scrollToActiveSong()
+        }
+
+        // When prev song
+        prevBtn.onclick = function() {
+            if (_this.isRandom) {
+                _this.playRandomSong()
+            } else {
+                _this.prevSong()
+            }      
+
+            audio.play()
+            _this.render()
+        }
+
+        // When random song is on/off
+        randomBtn.onclick = function(e) {
+            if (_this.isRepeat) { repeatBtn.click() }
+            _this.isRandom = !_this.isRandom
+            _this.setConfig('isRandom', _this.isRandom)
+            randomBtn.classList.toggle('active', _this.isRandom)
+        }
+
+        // When repeat is on/off
+        repeatBtn.onclick = function(e) {
+            if (_this.isPlaying) { randomBtn.click() }
+            _this.isRepeat = !_this.isRepeat
+            _this.setConfig('isRepeat', _this.isRepeat)
+            repeatBtn.classList.toggle('active', _this.isRepeat)
+        }
+
+        // Handle next song when audio ended
+        audio.onended = function() {
+            if (_this.isRepeat) {
+                audio.play()
+            } else {
+                nextBtn.click()
+            }
+        }
+
+        // Listen on click into playlist
+        playlist.onclick = function(e) {
+            const songNode = e.target.closest('.song:not(.active)')
+
+            // Handle when song is clicked
+            if (songNode && !e.target.closest('.option')) {                
+                    _this.currentIndex = Number(songNode.dataset.index)
+                    _this.loadCurrentSong()
+                    _this.render()
+                    audio.play()
+            }
+
+            // Handle when option is clicked
+            if (e.target.closest('.option')) {
+                console.log("OPTION")
+            }
+        }
+    },
+    scrollToActiveSong: function() {
+        setTimeout(() => {
+            $('.song.active').scrollIntoView({
+                behavior: 'smooth',
+                block: 'nearest',
+            })
+        }, 500)
+    },
+    loadCurrentSong: function() {
+        heading.textContent = this.currentSong.name
+        cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`
+        audio.src = this.currentSong.path
+    },
+    loadConfig: function() {
+        console.log(this.config)
+        this.isRandom = this.config.isRandom
+        this.isRepeat = this.config.isRepeat
+    },
+    nextSong: function() {
+        this.currentIndex++
+        if (this.currentIndex >= this.songs.length) {
+            this.currentIndex = 0
+        }
+        this.loadCurrentSong()
+    },
+    prevSong: function() {
+        this.currentIndex--
+        if (this.currentIndex < 0) {
+            this.currentIndex = this.songs.length - 1
+        }
+        this.loadCurrentSong()
+    },
+    playRandomSong: function() {
+        let newIndex
+        do {
+            newIndex = Math.floor(Math.random() * this.songs.length)
+        } while (newIndex === this.currentIndex)
+
+        this.currentIndex = newIndex
+        this.loadCurrentSong()
+    },
+    start: function() {
+        // Assign config from config into app
+        this.loadConfig()
+
+        // define properties for object
+        this.defineProperties()
+
+        // listen/ handle DOM events
+        this.handleEvents()
+
+        // load first song into UI when app is running
+        this.loadCurrentSong()
+
+        // render playlist
+        this.render()
+
+        // Display current state of random & repeat btn
+        randomBtn.classList.toggle('active', this.isRandom)
+        repeatBtn.classList.toggle('active', this.isRepeat)
+    }
+}
+
+app.start()
